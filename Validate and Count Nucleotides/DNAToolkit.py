2308c2ba72b714f7d15340eb34105a7539c47341
Christopher Adetola <48267165+chrisseub@users.noreply.github.com>
2025-08-20 13:00:18 +0000

    changes

--- Validate and Count Nucleotides/DNAToolkit.py
+++ Validate and Count Nucleotides/DNAToolkit.py
@@ -24,3 +24,15 @@ def transcription(seq):
 def reverse_complement(seq):
     """Swapping Adenine with Thymine and  Guanine with Cytosine, Reversing newly generated strings"""
     return ''.join([DNA_ReverseComplement[nuc] for nuc in seq])
+
+def gc_content(seq):
+    """GC Content present in a DNA/RNA Sequence"""
+    return round((seq.count('C') + seq.count('G')) / len(seq) * 100)
+
+def gc_content_subsec(seq, k=20):
+    """GC Content for DNA/RNA of sub-length K, which is 20 by default, edit to your choice"""
+    res = []
+    for i in range(0, len(seq) - k + 1, k):
+        subseq = seq[i:i + k]
+        res.append(gc_content(subseq))
+    return res
--- Validate and Count Nucleotides/main.py
+++ Validate and Count Nucleotides/main.py
@@ -8,14 +8,21 @@ rndDNAStr = "".join(random.choice(Nucleotide) for _ in range(50))
 
 DNAStr = (validateSeq(rndDNAStr))
 
-#Firtst Tutorial Execution
+
+#Firtst Tutorial Execution, for NA Count
 print(f'\nSequence: {colored(DNAStr)}\n')
 print(f'[1] + Sequence Length: {len(DNAStr)}\n')
 print(f'[2] + Nucleotide Frequency: {countNucFrequency(DNAStr)}\n')
 
-#Second Tutorial Execution
+
+#Second Tutorial Execution, for transcription
 print(f'[3] + DNA to RNA Transcription: {colored(transcription(DNAStr))}\n')
 print(f"[4] + DNA String + Reverse Complement:\n5' {colored(DNAStr)} 3'")
 
 print(f"   {''.join(['|' for c in range(len(DNAStr))])}")
 print(f"3' {colored(reverse_complement(DNAStr))}  5'\n")
+
+
+#Third Tutorial Execution, for GC content determination
+print(f'[5] + GC Content: {gc_content(DNAStr)}%\n')
+print(f'[6] + GC Content in Subsection k=5': {gc_content_subsec(DNAStr, k=5)}\n')
