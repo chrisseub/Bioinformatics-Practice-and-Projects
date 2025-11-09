c3290800f4349c6ef7ec68cff233793da8abe072
Christopher Adetola <48267165+chrisseub@users.noreply.github.com>
2025-11-09 19:06:57 +0100

    UPDATE

--- Validate and Count Nucleotides/DNAToolkit.py
+++ Validate and Count Nucleotides/DNAToolkit.py
@@ -35,4 +35,83 @@ def gc_content_subsec(seq, k=20):
     for i in range(0, len(seq) - k + 1, k):
         subseq = seq[i:i + k]
         res.append(gc_content(subseq))
+
     return res
+
+  def translate_seq(self, init_pos=0):
+        """Translates a DNA sequence into an aminoacid sequence"""
+        if self.seq_type == "DNA":
+            return [DNA_Codons[self.seq[pos:pos + 3]] for pos in range(init_pos, len(self.seq) - 2, 3)]
+        elif self.seq_type == "RNA":
+            return [RNA_Codons[self.seq[pos:pos + 3]] for pos in range(init_pos, len(self.seq) - 2, 3)]
+
+    def codon_usage(self, aminoacid):
+        """Provides the frequency of each codon encoding a given aminoacid in a DNA sequence"""
+        tmpList = []
+        if self.seq_type == "DNA":
+            for i in range(0, len(self.seq) - 2, 3):
+                if DNA_Codons[self.seq[i:i + 3]] == aminoacid:
+                    tmpList.append(self.seq[i:i + 3])
+
+        elif self.seq_type == "RNA":
+            for i in range(0, len(self.seq) - 2, 3):
+                if RNA_Codons[self.seq[i:i + 3]] == aminoacid:
+                    tmpList.append(self.seq[i:i + 3])
+
+        freqDict = dict(Counter(tmpList))
+        totalWight = sum(freqDict.values())
+        for seq in freqDict:
+            freqDict[seq] = round(freqDict[seq] / totalWight, 2)
+        return freqDict
+ def gen_reading_frames(self):
+        """Generate the six reading frames of a DNA sequence, including reverse complement"""
+        frames = []
+        frames.append(self.translate_seq(0))
+        frames.append(self.translate_seq(1))
+        frames.append(self.translate_seq(2))
+        tmp_seq = bio_seq(self.reverse_complement(), self.seq_type)
+        frames.append(tmp_seq.translate_seq(0))
+        frames.append(tmp_seq.translate_seq(1))
+        frames.append(tmp_seq.translate_seq(2))
+        del tmp_seq
+        return frames
+
+    def proteins_from_rf(self, aa_seq):
+        """Compute all possible proteins in an aminoacid seq and return a list of possible proteins"""
+        current_prot = []
+        proteins = []
+        for aa in aa_seq:
+            if aa == "_":
+                # STOP accumulating amino acids if _ - STOP was found
+                if current_prot:
+                    for p in current_prot:
+                        proteins.append(p)
+                    current_prot = []
+            else:
+                # START accumulating amino acids if M - START was found
+                if aa == "M":
+                    current_prot.append("")
+                for i in range(len(current_prot)):
+                    current_prot[i] += aa
+        return proteins
+
+    def all_proteins_from_orfs(self, startReadPos=0, endReadPos=0, ordered=False):
+        """Compute all possible proteins for all open reading frames"""
+        """Protine Search DB: https://www.ncbi.nlm.nih.gov/nuccore/NM_001185097.2"""
+        """API can be used to pull protein info"""
+        if endReadPos > startReadPos:
+            tmp_seq = bio_seq(
+                self.seq[startReadPos: endReadPos], self.seq_type)
+            rfs = tmp_seq.gen_reading_frames()
+        else:
+            rfs = self.gen_reading_frames()
+
+        res = []
+        for rf in rfs:
+            prots = self.proteins_from_rf(rf)
+            for p in prots:
+                res.append(p)
+
+        if ordered:
+            return sorted(res, key=len, reverse=True)
+        return res
